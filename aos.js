291997ec601fb0c27030b6027f3e7fa177b2f02f
piojeet <manjhipiojeet@gmail.com>
2023-10-06 02:04:33 +0530

    first commit

---
+++ aos.js
@@ -0,0 +1,12 @@
+// // JavaScript to handle the scrolling and SVG animation
+// const svgElement = document.getElementById('mySvg');
+// const svgRect = svgElement.getBoundingClientRect();
+
+// window.addEventListener('scroll', () => {
+//   const scrollPosition = window.scrollY + window.innerHeight;
+
+//   // Check if the bottom of the SVG is in the viewport
+//   if (scrollPosition > svgRect.top) {
+//     svgElement.style.opacity = '1'; // Make the SVG visible
+//   }
+// });
